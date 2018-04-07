4118ce0bdef9eb0e14b63f27b432f8c7d9a4e7c2
tmasjc <thomas.jcng@gmail.com>
2018-04-07 11:48:57 +0800

    Deal with application key

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -1,9 +1,16 @@
 library(GeocodeCN)
 context("Basic")
 
+skip_if_no_auth <- function() {
+    if (identical(Sys.getenv("amap_key"), "")) {
+        skip("No authentication available")
+    }
+}
+
 test_that(
     "Simple geocode an address",
     {
+        skip_if_no_auth()
         # Test wrapper
         expect_error(geocode("北京市朝阳区朝阳首府"))
         expect_equal(geocode("北京市朝阳区朝阳首府", key = Sys.getenv("amap_key"))$status, "1")
